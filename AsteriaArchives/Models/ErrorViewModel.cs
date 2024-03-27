0fb08e119a55e3ba5fbca3ceb17fb6c2bcf343da
Sarah <sarah.matta@enformion.com>
2024-03-27 15:46:20 -0400

    init all

---
+++ AsteriaArchives/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AsteriaArchives.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
